25d7d4d657a0df1039dbfc58b066e193bf253410
Felipe <felipemarostega.ff@gmail.com>
2025-06-14 16:31:53 -0300

    cleanup: removing unecessary comments

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,3 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import path from 'path';
-// import tailwindcss from '@tailwindcss/vite';
-
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     tailwindcss(),
-//   ],
-//   css: {
-//     postcss: './postcss.config.js', 
-//   },
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, 'src'),
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
